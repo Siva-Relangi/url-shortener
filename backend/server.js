3de90f2e19fe52eb1146b43e4fb897d1314672ea
Siva-Relangi <143422066+Siva-Relangi@users.noreply.github.com>
2025-08-14 22:39:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite default port
+  origin: 'https://url-shortener-frontend-tg3v.onrender.com', // Vite default port
   credentials: true
 }));
 
